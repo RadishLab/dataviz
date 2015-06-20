247238b161a4c983b3411ffb365d818e066b836a
Edward Wisniewski <edward@radishlab.com>
2015-06-20 15:23:44 -0400

    Installed libsass and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 
 gulp.task('browser-sync', function() {
@@ -10,4 +12,20 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('default', ['browser-sync']);
+gulp.task('sass', function() {
+  gulp.src('assets/sass/*.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      indentedSyntax: true,
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/assets/css'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('default', [
+  'browser-sync',
+  'sass'
+]);
